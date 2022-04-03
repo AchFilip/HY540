64eeceea6934bcf5fc8bfaedd4c59734ee61bd16
Antonis Prinianakis <antoprinia@gmail.com>
2022-04-03 22:39:17 +0300

    added treetags in a header file

--- AST/TreeVisitor.h
+++ AST/TreeVisitor.h
@@ -1,26 +1,20 @@
-#define AST_TAG_IF			"if"
-#define AST_TAG_IF_COND		"if_cond"
-#define AST_TAG_IF_STMT		"if_stmt"
-#define AST_TAG_WHILE_COND	"while_cond"
-#define AST_TAG_WHILE_STMT	"while_stmt"
-#define AST_TAG_WHILE		"while"
-#define AST_TAG_FOR			"for"
 
 #pragma once
 #include "Object.h"
+#include "TreeTags.h"s
 
 class TreeVisitor {
-	public:
-	virtual void VisitVarDecl (const Object& node) {} 
-	virtual void VisitFuncDef (const Object& node) {} 
-	virtual void VisitBlock   (const Object& node) {} 
-	virtual void VisitIf      (const Object& node) {} 
-	virtual void VisitWhile   (const Object& node) {} 
-	virtual void VisitExpr    (const Object& node) {} 
-	virtual void VisitStmt    (const Object& node) {} 
-	// ... all the rest visitor methods...
+ public:
+  virtual void VisitVarDecl(const Object& node) {}
+  virtual void VisitFuncDef(const Object& node) {}
+  virtual void VisitBlock(const Object& node) {}
+  virtual void VisitIf(const Object& node) {}
+  virtual void VisitWhile(const Object& node) {}
+  virtual void VisitExpr(const Object& node) {}
+  virtual void VisitStmt(const Object& node) {}
+  // ... all the rest visitor methods...
 
-	virtual TreeVisitor* Clone (void) const = 0;
-	TreeVisitor(void);
-	TreeVisitor (const TreeVisitor&)=default;
+  virtual TreeVisitor* Clone(void) const = 0;
+  TreeVisitor(void);
+  TreeVisitor(const TreeVisitor&) = default;
 };
--- AST/Value.h
+++ AST/Value.h
@@ -3,84 +3,81 @@
 // all nodes have a 'type' and 'line' key
 #pragma once
 
-#define AST_TAG_TYPE_KEY    "type"
-#define AST_TAG_LINE_KEY    "line"
-#define AST_TAG_VAR         "var"
-#define AST_TAG_LVALUE      "lvalue"
-#define AST_TAG_ID          "id"
-#define AST_TAG_STMT        "stmt"
-
-#include <tuple>
 #include <string>
+#include <tuple>
+
+#include "TreeTags.h"
 
 class Object;
 
-enum NIL{_NIL_ = 0};
-typedef void (*LibraryFunc) (Object&);
-class Value{
-    public:
-        enum Type{
-            UndefType =  0,
-            NumberType,
-            BooleanType,
-            StringType,
-            ObjectType,
-            ProgramFunctionType,
-            LibraryFunctionType,
-            NativePtrType,
-            NilTypem
-        };
-        using NativePtr = const std::tuple<void*, std::string>;
-    private:
-        Type type;
-        union{
-            double  numVal;
-            bool    boolVal;
-            char*   strVal;
-            char*   libFuncVal;
-            struct {
-                Object* ast;
-                Object* closure;
-            } progFuncVal;
-            struct{
-                void*   ptr;
-                char*   typeId;
-            } nativePtrVal;
-        } data;
-    public:
-        Value(void* ptr, const std::string& type);
-        Value(const NativePtr&);
-        Value(const std::string&);
-        Value(double);
-        Value(LibraryFunc);
-        Value(enum NIL);
-        Value(const Object&);
-        Value(const Value&);
-        Value(void);
-        ~Value();
-    public:
-        Type                GetType(void) const;
-        double              ToNumber(void) const;
-        bool                ToBool(void) const;
-        const std::string&  ToString(void) const;
-        const Object*	    ToObject (void) const;
-        const Object*	    ToProgramFunctionAST (void) const;
-        const Object*	    ToProgramFunctionClosure (void) const;
-        Object*		        ToObject_NoConst (void) const;
-        Object*		        ToProgramFunctionAST_NoConst (void) const;
-        Object*		        ToProgramFunctionClosure_NoConst  (void) const;
-        LibraryFunc	        ToLibraryFunction (void) const;
-        void*		        ToNativePtr (void) const;
-        const char*	        ToNativeTypeId (void) const;
-        void		        FromNil (void);
-        void		        FromString (const std::string&);
-        void		        FromNativePtr  (void* ptr, const std::string& type);
-        void		        FromNativePtr  (const NativePtr&); // alt style
-        operator		    bool() const; // true test
-        Value* 		        Clone (void) const;
-        const Value 	    operator=(const Value&);
+enum NIL { _NIL_ = 0 };
+typedef void (*LibraryFunc)(Object&);
+class Value {
+ public:
+  enum Type {
+    UndefType = 0,
+    NumberType,
+    BooleanType,
+    StringType,
+    ObjectType,
+    ProgramFunctionType,
+    LibraryFunctionType,
+    NativePtrType,
+    NilTypem
+  };
+  using NativePtr = const std::tuple<void*, std::string>;
 
-        // - here helper 'bool Is<type>(void)' const methods
-        // - here similar 'From<type>(<type>)' methods
+ private:
+  Type type;
+  union {
+    double numVal;
+    bool boolVal;
+    char* strVal;
+    char* libFuncVal;
+    struct {
+      Object* ast;
+      Object* closure;
+    } progFuncVal;
+    struct {
+      void* ptr;
+      char* typeId;
+    } nativePtrVal;
+  } data;
 
-}; 
+ public:
+  Value(void* ptr, const std::string& type);
+  Value(const NativePtr&);
+  Value(const std::string&);
+  Value(double);
+  Value(LibraryFunc);
+  Value(enum NIL);
+  Value(const Object&);
+  Value(const Value&);
+  Value(void);
+  ~Value();
+
+ public:
+  Type GetType(void) const;
+  double ToNumber(void) const;
+  bool ToBool(void) const;
+  const std::string& ToString(void) const;
+  const Object* ToObject(void) const;
+  const Object* ToProgramFunctionAST(void) const;
+  const Object* ToProgramFunctionClosure(void) const;
+  Object* ToObject_NoConst(void) const;
+  Object* ToProgramFunctionAST_NoConst(void) const;
+  Object* ToProgramFunctionClosure_NoConst(void) const;
+  LibraryFunc ToLibraryFunction(void) const;
+  void* ToNativePtr(void) const;
+  const char* ToNativeTypeId(void) const;
+  void FromNil(void);
+  void FromString(const std::string&);
+  void FromNativePtr(void* ptr, const std::string& type);
+  void FromNativePtr(const NativePtr&);  // alt style
+  operator bool() const;                 // true test
+  Value* Clone(void) const;
+  const Value operator=(const Value&);
+
+  // - here helper 'bool Is<type>(void)' const methods
+  // - here similar 'From<type>(<type>)' methods
+};
